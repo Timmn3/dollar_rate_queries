8aa1016e0e48578fe1209b2964c9045de2dba5cf
Tim <timmn@mail.ru>
2024-04-23 20:57:13 +0300

    dollar_rate.py

--- handlers/users/dollar_rate.py
+++ handlers/users/dollar_rate.py
@@ -7,6 +7,7 @@ from request.get_course import get_currency_rate
 
 @dp.message_handler(text='/dollar_rate')
 async def dollar_rate(message: types.Message):
+    user_id = int(message.from_user.id)
     # Отправляем пользователю текущий курс доллара
-    text = get_currency_rate()
+    text = await get_currency_rate(user_id)
     await message.answer(text)
