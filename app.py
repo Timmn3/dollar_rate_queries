5ed08b1653f08be121efe482a5ea592d9e2d550c
Tim <timmn@mail.ru>
2024-04-24 11:35:07 +0300

    refactoring

--- app.py
+++ app.py
@@ -1,40 +1,42 @@
-from loader import scheduler
+from loader import scheduler, db
 from scheduler.by_time import schedule_jobs
+from loguru import logger
+from utils.db_api.db_gino import on_startup
+from bot_send.notify_admins import on_startup_notify
+from bot_send.set_bot_commands import set_default_commands
+from aiogram import executor
+from handlers import dp as dp_handler
 
 
-async def on_startup(dpr):
-    from loguru import logger
+async def startup(dpr):
+    """
+    Функция, вызываемая при запуске бота.
+    """
+    # Инициализация логирования в файл
     logger.add("file.log", format="{time} {level} {message}", level="DEBUG", rotation="50 MB", compression="zip")
 
-    from loader import db
-    from utils.db_api.db_gino import on_startup
-    # print('Подключение к PostgreSQL')
+    # Инициализация базы данных
     await on_startup(db)
 
-    # print('Удаление базы данных')
+    # Удаление базы данных
     # await db.gino.drop_all()
 
-    # print('создание таблиц')
+    # Создание всех таблиц и запись лога о запуске
     await db.gino.create_all()
     logger.info('Бот запущен')
 
-    # импортирует функцию, которая отправляет сообщение о запуске бота всем администраторам
-    from bot_send.notify_admins import on_startup_notify
+    # Отправка уведомления об запуске бота администраторам
     await on_startup_notify(dpr)
 
-    # импортирует функцию, которая устанавливает команды бота
-    from bot_send.set_bot_commands import set_default_commands
+    # Установка стандартных команд бота
     await set_default_commands(dpr)
 
-    # запускаем парсинг по времени
+    # Запуск планировщика
     await schedule_jobs()
 
 
 if __name__ == '__main__':
-    from aiogram import executor  # импортируем executor для запуска поллинга
-    from handlers import dp  # из хендлеров импортируем dp
-
     scheduler.start()
-    executor.start_polling(dp, on_startup=on_startup)
+    executor.start_polling(dp_handler, on_startup=startup)
 
 
